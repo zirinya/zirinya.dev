279f6b2e0c1bb6526789775d679adca9228dcb5c
may <sirinya.hwanphet@gmail.com>
2022-04-28 14:35:56 +0700

    add rss config and prism config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,15 +12,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        "icon": "src/static/favicon.ico"
+        icon: "src/static/favicon.ico",
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-
         path: `${__dirname}/blog/`,
-        name: `blog`
+        name: `blog`,
       },
     },
     {
@@ -31,8 +30,79 @@ module.exports = {
         // GitHub Flavored Markdown mode (default: true)
         gfm: true,
         // Plugins configs
-        plugins: [],
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: true,
+              },
+            },
+          },
+        ],
       },
     },
-  ]
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  desc: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                });
+              });
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 30,
+                sort: { order: DESC, fields: [frontmatter___date] },
+              
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { 
+                      slug 
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            output: "/rss.xml",
+            title: "RSS Feed",
+          },
+        ],
+      },
+    },
+  ],
 };
