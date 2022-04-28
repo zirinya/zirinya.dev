12eeef0369fb95ee27e985b6135fff9025f1ab3d
may <sirinya.hwanphet@gmail.com>
2022-04-28 14:32:45 +0700

    add notfound page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+
+function NotFound() {
+    return (
+        <Layout title="404: Page not found">
+            404
+        </Layout>
+    )
+}
+export default NotFound
