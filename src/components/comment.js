fcdde89078a1d9ea01c1e69b09fd7a87f8fa8941
may <sirinya.hwanphet@gmail.com>
2022-04-29 18:00:07 +0700

    update comment section

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,46 +1,39 @@
-import React, { useEffect } from 'react'
-const Giscus = ({ mapping }) => {
+import React, { useEffect } from "react";
+const Giscus = () => {
 
-    const COMMENTS_ID = 'comments-container'
-    const config = {
-        repo: 'zirinya/zirinya.dev',
-        repoId: 'R_kgDOHPuWnA',
-        category: '',
-        categoryId: '',
-        reactions: '1',
-        metadata: '0',
-        theme: 'light'
+  const COMMENTS_ID = "comments-container";
+  useEffect(() => {
+    function LoadComments() {
+      const script = document.createElement("script");
+      script.src = "https://giscus.app/client.js";
+      script.setAttribute("data-repo", "zirinya/zirinya.dev");
+      script.setAttribute("data-repo-id", "R_kgDOHPuWnA");
+      script.setAttribute("data-category", "General");
+      script.setAttribute("data-category-id", "DIC_kwDOHPuWnM4CO16T");
+      script.setAttribute("data-mapping", "title");
+      script.setAttribute("data-reactions-enabled", "1");
+      script.setAttribute("data-emit-metadata", "0");
+      script.setAttribute("data-theme", "light");
+      script.setAttribute("data-input-position", "bottom");
+      script.setAttribute("data-lang", "en");
+      script.setAttribute("crossorigin", "anonymous");
+      script.async = true;
+      const comments = document.getElementById(COMMENTS_ID);
+      if (comments) comments.appendChild(script);
+      return () => {
+        const comments = document.getElementById(COMMENTS_ID);
+        if (comments) comments.innerHTML = "";
+      };
     }
-    useEffect(() => {
-        function LoadComments() {
-            const script = document.createElement('script')
-            script.src = 'https://giscus.app/client.js'
-            script.setAttribute('data-repo', config.repo)
-            script.setAttribute('data-repo-id', config.repositoryId)
-            script.setAttribute('data-category', config.category)
-            script.setAttribute('data-category-id', config.categoryId)
-            script.setAttribute('data-mapping', mapping)
-            script.setAttribute('data-reactions-enabled', config.reactions)
-            script.setAttribute('data-emit-metadata', config.metadata)
-            script.setAttribute('data-theme', config.theme)
-            script.setAttribute('crossorigin', 'anonymous')
-            script.async = true
+    LoadComments();
+  });
 
-            const comments = document.getElementById(COMMENTS_ID)
-            if (comments) comments.appendChild(script)
+  return (
+    <div>
+      <div className="giscus" id={COMMENTS_ID} />
+    </div>
+  );
 
-            return () => {
-                const comments = document.getElementById(COMMENTS_ID)
-                if (comments) comments.innerHTML = ''
-            }
-        }
-        LoadComments()
-    });
-    return (
-        <div >
-            <div className="giscus" id={COMMENTS_ID} />
-        </div>
-    )
-}
+};
 
-export default Giscus
+export default Giscus;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import { Link, graphql } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 const Home = ({ data }) => {
   return (
     <Layout pageTitle="Home | zirinya.dev">
